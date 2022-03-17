7c18951604caa0e6ccdf912031c577c101146561
entrapta-dev <58216214+entrapta-dev@users.noreply.github.com>
2022-03-17 09:38:10 +0100

    polybar launch fix

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-DIR="$HOME/.config/polybar"
-
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main -C "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
